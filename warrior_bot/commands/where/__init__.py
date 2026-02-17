7c1945190c72d2a5913c3de1494c2c4ed4c13edf
Nick Leskie <hn8288@wayne.edu>
2026-02-17 11:26:07 -0500

    Added to where command
    Co-authored-by: Ryguy455 <he0405@wayne.edu>
    Co-authored-by: Nick Leskie <hn8288@wayne.edu>
    Co-authored-by: Elma Okanovic <hg6025@wayne.edu>

--- warrior_bot/commands/where/__init__.py
+++ warrior_bot/commands/where/__init__.py
@@ -1,9 +1,31 @@
 """Where command implementation."""
 
 import click
-
+import requests
+from bs4 import BeautifulSoup
 
 @click.command()
-def where():
+@click.argument("name")
+def where(name):
     """Where command."""
+
+    click.echo("Executing where command")
+
+    url = f"https://wayne.edu/people?type=people&q={name.replace(' ','+')}"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,"html.parser")
+
+    instructors = soup.find_all("td",{"data-label": "Name"})
+
+    if not instructors:
+        click.echo("No Instructor Found. Possible Issues: "
+                   "\n - Incorrect Spelling"
+                   "\n - Instructor may be new"
+                   "\n - Instructor may be a Teacher Assistant"
+                   "\n Please try again using wb where")
+        return
+
+
+
     click.echo("Executing where command")
