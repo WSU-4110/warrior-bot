869b282a8b31ab9903c1858e88b89d4d25ccba5a
Nick Leskie <hn8288@wayne.edu>
2026-02-21 15:05:58 -0500

    Fixed white space issue. Also fixed instructors not being found

--- warrior_bot/commands/where/__init__.py
+++ warrior_bot/commands/where/__init__.py
@@ -1,22 +1,26 @@
 """Where command implementation."""
 
+
 import click
 import requests
 from bs4 import BeautifulSoup
 
 @click.command()
-@click.argument("name")
-def where(name):
+@click.argument("first_name")
+@click.argument("last_name")
+def where(first_name, last_name):
     """Where command."""
 
-    click.echo("Executing where command")
+    click.echo("Executing where command...")
 
-    url = f"https://wayne.edu/people?type=people&q={name.replace(' ','+')}"
+    url = f"https://wayne.edu/people?type=people&q={first_name}+{last_name}"
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text,"html.parser")
 
-    instructors = soup.find_all("td",{"data-label": "Name"})
+
+    instructors = [row.find("td").get_text(strip=True)
+                   for row in soup.select("table.table-stack tbody tr")]
 
     if not instructors:
         click.echo("No Instructor Found. Possible Issues: "
@@ -26,6 +30,21 @@ def where(name):
                    "\n Please try again using wb where")
         return
 
+    count = len(instructors)
+    if count == 1:
+        click.echo("Placeholder to avoid error")
+        #Call the display info function
+    else:
+        #Could change to allow the user to select instructor directly
+        click.echo(f"{count} instructors found. Please insert the name using wb where")
+        for instructor in instructors:
+            click.echo(f" - {instructor}")
+
+    """End code for where command for Finding Instructors"""
 
 
-    click.echo("Executing where command")
+
+
+
+#Additional Functions for more simple code
+#def displayInstructorInfo(first_name,last_name,soup):
