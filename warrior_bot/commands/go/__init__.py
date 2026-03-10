227cf7678647a0e77c78bf1ba8ac778194ff3baa
Elma Okanovic <hg6025@wayne.edu>
2026-03-10 09:53:11 -0400

    Assignment 4 -- Design Pattern 'Command'

--- warrior_bot/commands/go/__init__.py
+++ warrior_bot/commands/go/__init__.py
@@ -4,28 +4,26 @@ import click
 import webbrowser
 
 class URL_Command:
-    def __init__(self, name, url)P:
+    def __init__(self, name, url):
         self.url = url;
         self.name = name;
     def execute(self):
         click.echo("Executing go command.")
-        click.echo("Taking user to {self.name}.")
+        click.echo("Taking user to {self.name}!")
+        webbrowser.open(self.url, new=1, autoraise=True)
+
+commands = {
+  "academica": URL_Command("WSU Academica", "https://academica.aws.wayne.edu/"),
+  "library": URL_Command("WSU Library", "https://library.wayne.edu/"),
+  "bookstore": URL_Command("WSU Bookstore", "https://waynestatebookstore.com/")
+}
 
 @click.command()
 @click.argument("resource")
 def go(resource):
-    """Go command."""
-    click.echo("Executing go command")
-    
-    if resource == "academica":
-        click.echo("Taking user to WSU Academica site!")
-        url = 'http://academica.aws.wayne.edu/'
-        webbrowser.open(url, new=1, autoraise=True)
-    elif resource == "library":
-        click.echo("Taking user to WSU Library site!")
-        url = 'https://library.wayne.edu/'
-        webbrowser.open(url, new=1, autoraise=True)
-    elif resource == "bookstore":
-        click.echo("Taking user to WSU Bookstore site!")
+    """Go command.""
+    comd = commands.get(resource)
+    if cmd:
+        cmd.execute()
     else:
         click.echo("invalid command")
