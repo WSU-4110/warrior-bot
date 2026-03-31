d11fa42b089de0ec8b8663cdd18721c584c8ec87
R <he0405@wayne.edu>
2026-03-31 11:10:51 -0400

    -Added help command to book command -Made further tweaks to information display on the help menu

--- warrior_bot/commands/go/__init__.py
+++ warrior_bot/commands/go/__init__.py
@@ -31,16 +31,16 @@ def go(resource):
 
 class goHelpCommand(click.Command): #Help command information
     def format_help(self, ctx, formatter):
-        formatter.write_text("Usage: wb go RESOURCE")
-        formatter.write_paragraph()
-
         formatter.write_text("Go Command: Open a supported WSU website.")
         formatter.write_paragraph()
 
+        formatter.write_text("Usage: wb go [RESOURCE]")
+        formatter.write_paragraph()
+
         formatter.write_text("RESOURCES:")
-        formatter.write_text("  academica - Opens your browser to the Academica website")
-        formatter.write_text("  library  -  Opens your browser to the WSU Library website")
-        formatter.write_text("  bookstore - Opens your browser to the WSU Bookstore website")
+        formatter.write_text("  academica - Opens your browser to the Academica website.")
+        formatter.write_text("  library  -  Opens your browser to the WSU Library website.")
+        formatter.write_text("  bookstore - Opens your browser to the WSU Bookstore website.")
         formatter.write_text("  help or --help - Show this message")
         formatter.write_paragraph()
 
