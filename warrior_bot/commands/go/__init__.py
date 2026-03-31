6075b966bfba28f164e6ded0d95ed52bf5a3cdc6
akrm al-hakimi <alhakimiakrmj@gmail.com>
2026-03-31 12:05:40 -0400

    fix lints

--- warrior_bot/commands/go/__init__.py
+++ warrior_bot/commands/go/__init__.py
@@ -1,21 +1,26 @@
 """Go command implementation."""
 
-import click
 import webbrowser
 
+import click
+
+
 class URL_Command:
-    def __init__(self,name, url):
-        self.name = name;
-        self.url = url;
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
     def execute(self):
         click.echo("Executing go command.")
         webbrowser.open(self.url, new=1, autoraise=True)
 
+
 commands = {
-  "academica": URL_Command("WSU Academica", "https://academica.aws.wayne.edu/"),
-  "library": URL_Command("WSU Library", "https://library.wayne.edu/"),
-  "bookstore": URL_Command("WSU Bookstore", "https://waynestatebookstore.com/")
+    "academica": URL_Command("WSU Academica", "https://academica.aws.wayne.edu/"),
+    "library": URL_Command("WSU Library", "https://library.wayne.edu/"),
+    "bookstore": URL_Command("WSU Bookstore", "https://waynestatebookstore.com/"),
 }
+
 
 @click.command()
 @click.argument("resource")
