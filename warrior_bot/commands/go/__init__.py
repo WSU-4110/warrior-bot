7e54a663f3407ac4213c120bf4c64465e31a6e76
R <he0405@wayne.edu>
2026-03-31 10:38:54 -0400

    -Changed how help command works internally -Added full help command functionality

--- warrior_bot/commands/go/__init__.py
+++ warrior_bot/commands/go/__init__.py
@@ -1,8 +1,6 @@
-"""Go command implementation."""
-
 import webbrowser
-
 import click
+from warrior_bot.commands.help import showHelp
 
 
 class URL_Command:
@@ -30,3 +28,40 @@ def go(resource):
         cmd.execute()
     else:
         click.echo("Invalid command")
+
+class goHelpCommand(click.Command): #Help command information
+    def format_help(self, ctx, formatter):
+        formatter.write_text("Usage: wb go RESOURCE")
+        formatter.write_paragraph()
+
+        formatter.write_text("Go Command: Open a supported WSU website.")
+        formatter.write_paragraph()
+
+        formatter.write_text("RESOURCES:")
+        formatter.write_text("  academica - Opens your browser to the Academica website")
+        formatter.write_text("  library  -  Opens your browser to the WSU Library website")
+        formatter.write_text("  bookstore - Opens your browser to the WSU Bookstore website")
+        formatter.write_text("  help or --help - Show this message")
+        formatter.write_paragraph()
+
+        formatter.write_text("Help Menu:")
+        formatter.write_text("  wb help or wb --help")
+
+
+@click.command(cls=goHelpCommand)
+@click.argument("resource", required=False)
+@click.pass_context
+def go(ctx, resource):
+    showHelp(ctx, resource)
+    click.echo("Executing go command")
+
+    if resource == "academica":
+        click.echo("Taking user to WSU Academica site!")
+        webbrowser.open("http://academica.aws.wayne.edu/", new=1, autoraise=True)
+    elif resource == "library":
+        click.echo("Taking user to WSU Library site!")
+        webbrowser.open("https://library.wayne.edu/", new=1, autoraise=True)
+    elif resource == "bookstore":
+        click.echo("Taking user to WSU Bookstore site!")
+    else:
+        click.echo("invalid command")
--- warrior_bot/commands/help/__init__.py
+++ warrior_bot/commands/help/__init__.py
@@ -1,8 +1,19 @@
 import click
 
+
+def showHelp(ctx: click.Context, value: str | None) -> None:
+    if value is None or value.lower() == "help":
+        click.echo(ctx.get_help())
+        ctx.exit()
+
+
 @click.command(name="help")
 @click.pass_context
 def help(ctx: click.Context) -> None:
     """Shows this menu."""
+<<<<<<< HEAD
     # Show the top-level help for the CLI
     click.echo(ctx.parent.get_help() if ctx.parent else ctx.get_help())
+=======
+    click.echo(ctx.parent.get_help() if ctx.parent else ctx.get_help())
+>>>>>>> b41894f (-Changed how help command works internally)
