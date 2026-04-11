fbf439b63513db6e6edea4d987c01cab224f516c
akrm al-hakimi <alhakimiakrmj@gmail.com>
2026-04-11 00:39:40 -0400

    chore(release): cut 0.2.0 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="warrior-bot",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "beautifulsoup4",
