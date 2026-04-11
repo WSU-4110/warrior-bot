491f35c9d433d3e4adcfdc57b101b2bf9e614629
akrm al-hakimi <alhakimiakrmj@gmail.com>
2026-04-11 00:47:45 -0400

    fix: include locations.json in package data

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="warrior-bot",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
+    package_data={
+        "warrior_bot": ["commands/where/data/*.json", "data/*.json"],
+    },
     install_requires=[
         "beautifulsoup4",
         "click",
